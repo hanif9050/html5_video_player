5000a50dd2e0b1abff543e19823f1247c9634058
hanif9050 <hanif.ctg9050@gmail.com>
2022-06-05 12:04:49 +0600

    complete css

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get our Elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButton = player.querySelectorAll("[data-skip]");
